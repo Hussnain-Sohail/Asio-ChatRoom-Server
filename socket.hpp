4e5b355cd9413b5e3fadfb38c38fcd7fcd474ccf
hussnain sohail <h63097834@gmail.com>
2026-08-05 13:13:20 +0500

    initial commit

---
+++ socket.hpp
@@ -0,0 +1,52 @@
+#include <boost/asio.hpp>
+#include <openssl/rand.h>
+#include <iostream>
+
+#ifndef SOCKET
+#define SOCKET
+
+using boost::asio::ip::tcp;
+class Socket
+{
+private:
+    tcp::socket socket;
+    std::string socket_id;
+
+    void assign_id()
+    {
+        unsigned char id[16];
+        if (RAND_bytes(id, sizeof(id)) == 1)
+        {
+            char *readable_id = OPENSSL_buf2hexstr(id, sizeof(id));
+
+            socket_id = std::move(readable_id);
+        }
+        else
+            std::cout << "could not assign id" << std::endl;
+    }
+
+public:
+    Socket(boost::asio::io_context &io) : socket(io)
+    {
+        assign_id();
+    }
+
+    tcp::socket &give_socket()
+    {
+        return socket;
+    }
+
+    std::string give_socket_id() noexcept
+    {
+        return socket_id;
+    }
+
+    Socket(const Socket &) = delete;
+    Socket &operator=(const Socket &) = delete;
+
+    Socket(Socket &&) = delete;
+    Socket &operator=(Socket &&) = delete;
+
+    ~Socket() = default;
+};
+#endif
